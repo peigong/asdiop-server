67b9b37118f7df163c84d1477fbdaad878ac733a
周培公 <peigong@foxmail.com>
2018-02-02 17:46:30 +0800

    log

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ fs.readFile(versionPath, (err, data) => {
         throw err;
     }else{
         versionData = data;
-        logger.log(data);
+        logger.log(data.toString('utf-8'));
     }
 });
 
