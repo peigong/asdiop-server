4eb40bb1e0443a2a8d99403b397c41cda2bc119a
周培公 <peigong@foxmail.com>
2018-03-09 17:22:30 +0800

    lib

--- asdiop.cmd
+++ asdiop.cmd
@@ -1,3 +1,2 @@
 git pull
-npm install
 node server.js
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const net = require('net');
-const fs = require('fs');
 const path = require('path');
 
+const data = require('./lib/byte-data.js');
 const { host, port } = require('./config.json');
+
 const STATE = {
     INIT: 0,
     RUNING: 1,
@@ -20,38 +21,10 @@ server.listen(port, host, () => {
     logger.log(`Server listening on ${ address }:${ port }`);
 });
 
-let packages = [];
-let total = 0;
-
 const version = '4';
-const LENGTH = 1024;
 const versionPath = path.join(__dirname, '../../__data__', version);
-fs.readFile(versionPath, (err, data) => {
-    if(err){
-        logger.error(err);
-    }else{
-        let buf = Buffer.from(data);
-        let bufLength = buf.length;
-        total = Math.ceil(bufLength / LENGTH);
-        for(let i = 0; i < total; i++){
-            let tail = null;
-            let byteOffset = i * LENGTH;
-            let len = bufLength - byteOffset;
-            if(len > LENGTH){
-                len = LENGTH;
-            }else{
-                tail = Buffer.alloc(LENGTH - len);
-            }
-            let bufArr = [Buffer.from(buf, byteOffset, len)];
-            if(tail){
-                bufArr.push(tail);
-            }
-            packages.push(Buffer.concat(bufArr));
-            logger.log(`package index:${ i }`);
-        }
-        logger.log(`package total:${ total }`);
-    }
-});
+
+let { packages, total } = data.init(versionPath);
 
 server.on('connection', function(socket) {
     const remoteAddress = socket.remoteAddress;
